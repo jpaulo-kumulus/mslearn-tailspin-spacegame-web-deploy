13a918b39d20877eeaecaae9f0e00ef0366e4e9d
João Paulo oliveira <joao.oliveira@kumulus.com.br>
2020-07-30 16:23:00 -0300

    Add the Environment

---
+++ Terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "prefix" {
+    description = "The prefix used for all resources in this example"
+}
+
+variable "location" {
+    description = "The Azure location where all resources in this example should be created"
+}
+
+variable "subscription_id" {
+    description = "Azure Subscription ID to be used for billing"
+}
